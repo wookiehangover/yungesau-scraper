31e7aed13de2e25032098a2f710ee460606ec7a4
Sam Breed <samuel.breed@gmail.com>
2022-09-12 15:11:19 -0700

    check page before scraping

---
+++ check_page.py
@@ -0,0 +1,26 @@
+import requests
+import datetime
+
+def read_file(filename):
+    with open(filename, 'r') as f:
+        return f.read().strip()
+
+def write_file(filename, content):
+    with open(filename, 'w') as f:
+        f.write(content)
+
+def get_updated_at():
+    r = requests.get(url="https://yungesau.net/collections/tees-more.json")
+    data = r.json()
+    return datetime.datetime.fromisoformat(data['collection']['updated_at'])
+
+prev_time = datetime.datetime.fromisoformat(read_file('./updated_at.txt'))
+updated_at = get_updated_at()
+
+write_file('./updated_at.txt', updated_at.isoformat())
+
+if prev_time < updated_at:
+    print('updated')
+else:
+    print('not updated')
+    exit(1)
